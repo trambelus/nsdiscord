e2a7dcc60dbed8cbea138bb529f3dc0e9ce3187d
Trambelus <trambelus2@gmail.com>
2018-11-20 17:38:48 -0500

    Add main.py stubs

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from flask import Flask
+import os
+import sqlite3
+
+DB_FILE = "users.db3"
+
+def init_db(db_file):
+	pass
+
+@app.route("/<string:nation>/<int:issue>/<int:option>")
+def vote():
+	pass
+
+@app.route("/<string:nation>/register")
+def register():
+	pass
+
